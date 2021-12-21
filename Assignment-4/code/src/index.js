26c35c1e290a6ac651a9bc2ef2cf09e50a8d96df
wellimaj <adityajoshi3313@gmail.com>
2021-12-21 17:38:03 +0530

    final

--- Assignment-4/code/src/index.js
+++ Assignment-4/code/src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Postview from './Postview';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Postview from "./Postview";
+import reportWebVitals from "./reportWebVitals";
+import Header from "./Header"
+fetch("http://localhost:3004/user") //1
+  .then((response) => response.json()) //2
+  .then((res) => {
+    ReactDOM.render(
+      <React.StrictMode>
+        <Header></Header>
+        <Postview res={res} />
+      </React.StrictMode>,
+      document.getElementById("root")
+    ); //3
+  });
 
 
 
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Postview />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
